735aef835bd5ca844bdeeb27bea9bd293ce57dde
Tashi Carter <sc21ktoc@leeds.ac.uk>
2023-12-11 00:45:48 +0000

    Initial commit (Includes TASHI changes)

---
+++ src/main/java/comp3911/cwk2/Encryption.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryption 
+{
+    public static void encryptString(String input) throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+
+    byte[] messageDigest = md.digest(input.getBytes());
+
+    BigInteger bigInt = new BigInteger(1, messageDigest);
+
+    System.out.println(bigInt.toString(16));
+  }
+    public static void main(String[] args) throws Exception 
+    {
+        encryptString(args[0]);
+    }
+}
