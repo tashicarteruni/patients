72a200390e643001995f4acfb37df8f35d1bd43d
ed20mhl <ed20mhl@leeds.ac.uk>
2023-12-11 13:40:52 +0000

    SQL Inject and XSS attacks have been secured!

--- src/main/java/comp3911/cwk2/AppServlet.java
+++ src/main/java/comp3911/cwk2/AppServlet.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,17 +19,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import freemarker.core.HTMLOutputFormat;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
+import java.sql.PreparedStatement;
+
 @SuppressWarnings("serial")
 public class AppServlet extends HttpServlet {
 
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
-  private static final String AUTH_QUERY = "select * from user where username='%s' and password='%s'";
-  private static final String SEARCH_QUERY = "select * from patient where surname='%s' collate nocase";
+  private static final String AUTH_QUERY = "select * from user where username=? and password=?";
+  private static final String SEARCH_QUERY = "select * from patient where surname=? collate nocase";
 
   private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
   private Connection database;
@@ -48,6 +50,8 @@ public class AppServlet extends HttpServlet {
       fm.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
       fm.setLogTemplateExceptions(false);
       fm.setWrapUncheckedExceptions(true);
+      // Sets Output to HTML encoding, stopping XSS attacks
+      fm.setOutputFormat(HTMLOutputFormat.INSTANCE);
     }
     catch (IOException error) {
       throw new ServletException(error.getMessage());
@@ -118,18 +122,19 @@ public class AppServlet extends HttpServlet {
   private boolean authenticated(String username, String password) throws SQLException, NoSuchAlgorithmException {
     String hashedPassword = encryptString(password);
 
-    String query = String.format(AUTH_QUERY, username, hashedPassword);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
+    try (PreparedStatement stmt = database.prepareStatement(AUTH_QUERY) ) {
+      stmt.setString(1, username);
+      stmt.setString(2, hashedPassword);
+      ResultSet results = stmt.executeQuery();
       return results.next();
     }
   }
 
   private List<Record> searchResults(String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
-    String query = String.format(SEARCH_QUERY, surname);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
+    try (PreparedStatement stmt = database.prepareStatement(SEARCH_QUERY)) {
+      stmt.setString(1, surname);
+      ResultSet results = stmt.executeQuery();
       while (results.next()) {
         Record rec = new Record();
         rec.setSurname(results.getString(2));
